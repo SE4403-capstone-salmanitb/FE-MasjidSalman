359e35ae41c9179c5892bc3d31b877e1069af0d3
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2024-07-25 20:22:55 +0700

    update add bidang

---
+++ src/views/subpage/InputbidangView.vue
@@ -0,0 +1,307 @@
+<template>
+  <div class="profile">
+    <Sidebar />
+    <div class="card-container">
+      <div class="card mb-3" style="max-width: 1149px; max-height: fit-content">
+        <div class="kepala">
+          <p>Form Input</p>
+        </div>
+        <div class="container">
+          <form>
+            <div class="card-container1">
+              <div
+                class="card1 mb-3"
+                style="max-width: 1067px; max-height: 354"
+              >
+                <div class="form">
+                  <div class="mb-3">
+                    <label for="nama" class="form-label">Nama Program</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="nama1"
+                      v-model="formbidang.nama"
+                      required
+                    />
+                  </div>
+
+                  <div class="tombol">
+                    <button
+                      type="button"
+                      class="btn hapus-btn"
+                      style="
+                        display: none;
+                        background-color: #ff0000;
+                        margin-right: 14px;
+                      "
+                    >
+                      Hapus
+                    </button>
+                    <button
+                      type="button"
+                      class="btn tambah-btn"
+                      @click="SubmitForm"
+                    >
+                      Tambah
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tombol1">
+              <button
+                type="button"
+                class="btn"
+                @click="submitForm"
+                style="margin-bottom: 210px"
+              >
+                Simpan
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div v-if="isNotificationVisible" :class="notificationClass">
+        <div class="row">
+          <b-icon-check-circle
+            v-if="notificationType === 'success'"
+            style="
+              margin-right: 12px;
+              margin-left: 15px;
+              margin-top: 14px;
+              width: 30px;
+              height: 30px;
+            "
+          ></b-icon-check-circle>
+          <b-icon-exclamation-circle
+            v-if="notificationType === 'error'"
+            style="
+              margin-right: 12px;
+              margin-left: 15px;
+              margin-top: 14px;
+              width: 30px;
+              height: 30px;
+            "
+          ></b-icon-exclamation-circle>
+          <div class="notification-header">
+            <p class="notification-title">{{ notificationMessage }}</p>
+            <p class="notification-content">{{ notificationDetail }}</p>
+          </div>
+          <b-icon-x
+            @click="closeNotification"
+            style="
+              margin-left: 47px;
+              margin-right: 12px;
+              margin-top: 14px;
+              width: 30px;
+              height: 30px;
+            "
+          ></b-icon-x>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/SidebarView.vue";
+import axios from "@/lib/axios";
+
+export default {
+  data() {
+    return {
+      formbidang: {
+        nama: "",
+      },
+      programOptions: [],
+      showAddInput: false,
+      newProgram: "",
+      notificationMessage: "",
+      notificationDetail: "",
+      notificationType: "", // error, success
+      isNotificationVisible: false,
+    };
+  },
+  mounted() {},
+  methods: {
+    submitForm() {
+      console.log("Form Data:", this.formbidang);
+
+      axios
+        .post("/api/bidang", this.formbidang, {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("bearer"),
+          },
+        })
+        .then(() => {
+          // Handle successful response, e.g., show success message
+          this.notificationMessage = "Berhasil";
+          this.notificationDetail = "Data berhasil di upload";
+          this.notificationType = "success";
+          this.isNotificationVisible = true;
+          setTimeout(() => {
+            this.notificationMessage = "";
+            this.notificationDetail = "";
+            this.notificationType = "";
+            this.isNotificationVisible = false;
+          }, 10000); // Reset notification after 10 seconds
+          // Redirect to RKA page after successful submission
+          window.location.go(-1);
+        })
+        .catch((error) => {
+          // Handle error, e.g., show error message
+          this.notificationMessage = "Gagal";
+          this.notificationDetail =
+            "Gagal menginput data: " + error.response.data.message;
+          this.notificationType = "error";
+          this.isNotificationVisible = true;
+          console.error("Error:", error.response.data);
+        });
+    },
+    SubmitForm() {
+      console.log("Form Data:", this.formbidang);
+
+      axios
+        .post("/api/bidang", this.formbidang, {
+          headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("bearer"),
+          },
+        })
+        .then(() => {
+          // Handle successful response, e.g., show success message
+          this.notificationMessage = "Berhasil";
+          this.notificationDetail = "Data berhasil di upload";
+          this.notificationType = "success";
+          this.isNotificationVisible = true;
+          setTimeout(() => {
+            this.notificationMessage = "";
+            this.notificationDetail = "";
+            this.notificationType = "";
+            this.isNotificationVisible = false;
+          }, 10000); // Reset notification after 10 seconds
+          // Redirect to RKA page after successful submission
+        })
+        .catch((error) => {
+          // Handle error, e.g., show error message
+          this.notificationMessage = "Gagal";
+          this.notificationDetail =
+            "Gagal menginput data: " + error.response.data.message;
+          this.notificationType = "error";
+          this.isNotificationVisible = true;
+          console.error("Error:", error.response.data);
+        });
+    },
+    closeNotification() {
+      this.isNotificationVisible = false;
+    },
+  },
+  components: {
+    Sidebar,
+  },
+  computed: {
+    notificationClass() {
+      return {
+        notification: this.isNotificationVisible,
+        "notification-error": this.notificationType === "error",
+        "notification-success": this.notificationType === "success",
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.card-container {
+  padding-left: 260px; /* Lebar sidebar + jarak antara sidebar dan kartu */
+  padding-top: 33px;
+  overflow-y: auto;
+}
+
+.card-container1 {
+  overflow-y: auto;
+  margin-top: 32px;
+}
+
+.form {
+  margin-left: 16px;
+  margin-right: 9px;
+}
+
+.form .mb-3 .form-label {
+  margin-top: 12px;
+}
+
+.card-container1 .card1 {
+  background-color: #d9d9d9;
+  padding-left: 0%;
+  padding-top: 0%;
+}
+
+.card-container .card {
+  background-color: white;
+}
+
+.kepala {
+  background-color: #967c55;
+  height: 70px;
+  width: 1149px;
+}
+
+.kepala p {
+  font-size: 24px;
+  font-weight: 800;
+  color: white;
+  margin-top: 18px;
+  margin-left: 24px;
+}
+
+.teks {
+  margin-left: 19px;
+  margin-top: 30px;
+  color: black;
+  font-size: 15px;
+  font-weight: bold;
+}
+
+form {
+  margin-left: 45px;
+  margin-top: 57px;
+  margin-right: 36px;
+}
+
+.form-label {
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.card1 .tombol {
+  display: flex;
+  margin-top: 14px;
+  margin-right: 9px;
+}
+
+.card1 .tombol .btn {
+  background-color: #967c55;
+  font-size: 12px;
+  font-weight: bold;
+  height: 47px;
+  width: 104px;
+  color: white;
+  margin-bottom: 19px;
+}
+
+.tombol1 {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 32px;
+}
+
+.tombol1 .btn {
+  background-color: #967c55;
+  font-size: 16px;
+  font-weight: bolder;
+  height: 47px;
+  width: 104px;
+  color: white;
+}
+</style>
